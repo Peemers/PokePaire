8966412251eb6129b50cc11df15a5dec6cb73d58
Math Peeters <pmathieu.peeters@gmail.com>
2026-02-05 17:24:56 +0100

    commit 1 lol

---
+++ script.js
@@ -0,0 +1,147 @@
+let CarteRetourne = []
+let bloquage = false;
+let tentatives = 0;
+let seconde = 0;
+let interval;
+let jeuLance = false;
+let victoire = document.getElementById("victoire");
+
+//ajout du timer
+function demarrerTimer(){
+ if (jeuLance) return;
+ jeuLance = true;
+ interval = setInterval(() => {
+  seconde++;
+  document.querySelector("#timer").textContent = `${seconde}`;
+ }, 1000);
+}
+
+function arreterTimer(){
+ const cartesTrouvees = document.querySelectorAll(".estRetourne").length; // queryselector ne fonctionne pas ici, il faut all
+ if (cartesTrouvees === 4) {
+  clearInterval(interval);
+  victoire.textContent = `Bravo ! Tu as gagné en ${seconde} s et ${tentatives} tentatives! Tente un nouveau record !`;
+  const recommencer = document.createElement("button");
+  recommencer.classList.add("recommencer");
+  recommencer.textContent = "Recommencer";
+  victoire.appendChild(recommencer);
+ }
+}
+
+async function preparationJeu() {
+ try {
+  //preparation, selection de carte
+  const randomCalcul = Math.floor(Math.random() * 100);
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=2&offset=${randomCalcul}`);
+  const donnees = await response.json();
+
+  /*recup des images()*/
+  // tableau de donnees (donees.results) => vers un autre tableau de promesse
+  const promesse = donnees.results.map(p => fetch(p.url).then(response => response.json())) //<-- transformer la response en Json
+  console.log(promesse);
+  const pokemons = await Promise.all(promesse); // <- j'attends que toutes les promesses ok et lance tous les appels en parallele
+
+  //"DTO"
+  const CartesBase = pokemons.map(p => ({
+   nom: p.name,
+   image: p.sprites.front_default
+  })); //<<<-- saloperie de parentheses
+
+  // double cartes, je remplis CartesJeu avec 2x le meme tab. Merci mdn parceque [CartesBase, CartesBase] ça marche pas
+  let cartesJeu = [...CartesBase, ...CartesBase];
+
+
+  //mélange des cartes, utilisation d'un algo tout fait du net : Fisher-Yates.
+  //si je comprends bien il prend une carte dans i et la met au hasard dans J en partant de la derniere ensuite permute les tabs (destructuring comme c'est appelé) a se pencher dessus.
+  for (let i = cartesJeu.length - 1; i > 0; i--) {
+   const j = Math.floor(Math.random() * (i + 1));
+   [cartesJeu[i], cartesJeu[j]] = [cartesJeu[j], cartesJeu[i]];
+  }
+
+   //console.log(cartesJeu);
+
+   return cartesJeu;
+ } catch (error) {
+  console.log("erreur de création du jeu :", error)
+ }
+}
+//dom blablabla
+function AfficherCartes(cartes) {
+ const plateau = document.querySelector("#plateau");
+ plateau.textContent = "";
+
+ cartes.forEach(pok => {
+  const carte = document.createElement("div");
+  carte.classList.add("carte");
+  carte.dataset.name = pok.nom /*.dataset pour stocker les noms dans une variable et les comparer ensuite.
+                                  (Plus simple, remplace [if (carteA.querySelector('img').src === carteB.querySelector('img').src) etc..] astuce : Gemini.*/
+
+  const image = document.createElement("img");
+  image.src = pok.image;
+  image.alt = pok.nom;
+  image.classList.add("pokImg");
+
+  carte.appendChild(image); //image dans la carte
+  plateau.appendChild(carte); //cartes sur le plateau
+
+  carte.addEventListener("click", retourner); //<- lui il m'a fait chier, attention, pas en dehors.......
+ });
+}
+
+function retourner() {
+ if (bloquage) return;
+ if (this === CarteRetourne[0]) return; //this = qui vient d'etre cliquee dans ce cas. MDN
+ demarrerTimer();
+
+ this.classList.add("estRetourne"); //dom blabla
+ CarteRetourne.push(this); // ajout de la carte dans carteRetourne[]
+
+ //console.log(this.dataset.name);
+
+ if (CarteRetourne.length === 2){
+  tentatives++;
+  document.querySelector("#essais").textContent = tentatives;
+  verifPaire();
+ }
+}
+
+function verifPaire() {
+ let estPareil = CarteRetourne[0].dataset.name === CarteRetourne[1].dataset.name // je verifie si la carte dans [0] est pareil que [1] retourne true.
+
+ //console.log("Verif paire:", estPareil ? "ok" : "nok")
+
+ estPareil ? desactiverCartes() : cacherCartes();
+}
+
+function desactiverCartes() {
+ CarteRetourne[0].removeEventListener("click", retourner);
+ CarteRetourne[1].removeEventListener("click", retourner);
+
+ CarteRetourne = []; //clean tab
+ arreterTimer();
+}
+
+function cacherCartes() {
+ bloquage = true;
+
+ setTimeout(() => {
+  CarteRetourne[0].classList.remove("estRetourne");
+  CarteRetourne[1].classList.remove("estRetourne");
+
+  CarteRetourne = []; // clean du tab
+  bloquage = false;
+ }, 1000); // 1.5 seconde t'attente
+}
+// je prends le return "en attente"(promesse) de preparationJeu et la fonction pour afficher les cartes et magiiiie :) trop bien.
+preparationJeu().then(cartesJeu => {
+ if (cartesJeu) {
+  AfficherCartes(cartesJeu);
+ }
+})
+
+
+
+
+
+
+
